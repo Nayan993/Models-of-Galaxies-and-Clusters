595e11090b32bc7292b2e3f3cebbda112bdcea12
Anuj Sahu <anujsahu8847@gmail.com>
2025-11-06 14:43:23 +0530

    solved linkage and parameter issues

--- src/density.c
+++ src/density.c
@@ -1,19 +1,37 @@
-
 #include "density.h"
 #include <math.h>
+
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 
-
+// Plummer density profile
+// rho(r) = rho0 / (1 + (r/a)^2)^(5/2)
+// Used for globular clusters
 double plummer_density(double r, double rho0, double a)
 {
     return rho0 / pow(1.0 + (r*r)/(a*a), 2.5);
 }
 
+// Hernquist density profile  
+// rho(r) = (M/(2*pi)) * (a/r) * (1 + r/a)^(-3)
+// Used for galaxy bulges
 double hernquist_density(double r, double M, double a)
 {
+    if(r == 0.0) r = 1e-8;  // Avoid division by zero
     return (M / (2.0 * M_PI)) * (a / r) * pow(1.0 + (r / a), -3.0);
 }
 
-
+// Generic density function wrapper
+// Calls appropriate model based on ModelType
+double density(double r, ModelType model, double param1, double param2)
+{
+    switch(model) {
+        case PLUMMER:
+            return plummer_density(r, param1, param2);
+        case HERNQUIST:
+            return hernquist_density(r, param1, param2);
+        default:
+            return 0.0;
+    }
+}
--- src/density.h
+++ src/density.h
@@ -1,12 +1,31 @@
-
 #ifndef DENSITY_H
 #define DENSITY_H
-#define PLUMMER 0
-#define HERNQUIST 1
 
 #include <math.h>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+// Model identifiers
+typedef enum {
+    PLUMMER = 0,
+    HERNQUIST = 1
+} ModelType;
+
+// Star structure for 3D positions
+typedef struct {
+    double x;
+    double y;
+    double z;
+    double r;  // radial distance from origin
+} Star;
+
+// Density function prototypes
 double plummer_density(double r, double rho0, double a);
 double hernquist_density(double r, double M, double a);
 
-#endif
+// Generic density function wrapper
+double density(double r, ModelType model, double param1, double param2);
+
+#endif // DENSITY_H
