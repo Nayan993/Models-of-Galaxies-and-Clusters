5f587307ff6de075b1c78238eb9c2712af86bda1
Anuj Sahu <anujsahu8847@gmail.com>
2025-11-06 15:23:49 +0530

    solved several issues

--- src/density.c
+++ src/density.c
@@ -1,37 +1,64 @@
-#include "density.h"
 #include <math.h>
+#include <stdio.h>
+#include "density.h"
 
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
+#define PI 3.14159265358979323846
 
-// Plummer density profile
-// rho(r) = rho0 / (1 + (r/a)^2)^(5/2)
-// Used for globular clusters
-double plummer_density(double r, double rho0, double a)
-{
-    return rho0 / pow(1.0 + (r*r)/(a*a), 2.5);
+double plummer_density(double r, double rho0, double a) {
+    // Calculate (r/a) ratio
+    double ratio = r / a;
+    
+    // Calculate denominator: (1 + (r/a)²)^(5/2)
+    double denominator = pow(1.0 + ratio * ratio, 2.5);
+    
+    // Return density: ρ₀ / denominator
+    return rho0 / denominator;
 }
 
-// Hernquist density profile  
-// rho(r) = (M/(2*pi)) * (a/r) * (1 + r/a)^(-3)
-// Used for galaxy bulges
-double hernquist_density(double r, double M, double a)
-{
-    if(r == 0.0) r = 1e-8;  // Avoid division by zero
-    return (M / (2.0 * M_PI)) * (a / r) * pow(1.0 + (r / a), -3.0);
-}
-
-// Generic density function wrapper
-// Calls appropriate model based on ModelType
-double density(double r, ModelType model, double param1, double param2)
-{
-    switch(model) {
-        case PLUMMER:
-            return plummer_density(r, param1, param2);
-        case HERNQUIST:
-            return hernquist_density(r, param1, param2);
-        default:
-            return 0.0;
+double hernquist_density(double r, double M, double a) {
+    // Avoid division by zero at r=0
+    if (r < 1e-10) {
+        return 0.0;
     }
+    
+    // Calculate ratio r/a
+    double ratio = r / a;
+    
+    // Calculate: (M/2π)
+    double mass_factor = M / (2.0 * PI);
+    
+    // Calculate: (a/r)
+    double scale_factor = a / r;
+    
+    // Calculate: 1/(1+r/a)³
+    double falloff = 1.0 / pow(1.0 + ratio, 3.0);
+    
+    // Combine all factors
+    return mass_factor * scale_factor * falloff;
+}
+
+double find_max_density(ModelType model, double param1, double param2, double r_max) {
+    double max_dens = 0.0;
+    int steps = 1000;
+    
+    // Scan from r=0 to r=r_max to find maximum density
+    for (int i = 0; i <= steps; i++) {
+        double r = (i * r_max) / steps;
+        double dens;
+        
+        // Calculate density based on model type
+        if (model == PLUMMER) {
+            dens = plummer_density(r, param1, param2);
+        } else {
+            dens = hernquist_density(r, param1, param2);
+        }
+        
+        // Update maximum if current is larger
+        if (dens > max_dens) {
+            max_dens = dens;
+        }
+    }
+    
+    // Return max density with 10% safety margin
+    return max_dens * 1.1;
 }
--- src/density.h
+++ src/density.h
@@ -1,31 +1,15 @@
 #ifndef DENSITY_H
 #define DENSITY_H
 
-#include <math.h>
-
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
-// Model identifiers
 typedef enum {
-    PLUMMER = 0,
-    HERNQUIST = 1
+    PLUMMER,
+    HERNQUIST
 } ModelType;
 
-// Star structure for 3D positions
-typedef struct {
-    double x;
-    double y;
-    double z;
-    double r;  // radial distance from origin
-} Star;
-
-// Density function prototypes
 double plummer_density(double r, double rho0, double a);
+
 double hernquist_density(double r, double M, double a);
 
-// Generic density function wrapper
-double density(double r, ModelType model, double param1, double param2);
+double find_max_density(ModelType model, double param1, double param2, double r_max);
 
 #endif // DENSITY_H
